45859780b883fa667f0300381d073c49cab9af3b
mojvaf <mvafa44@gmail.com>
2017-03-05 19:33:09 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -9,7 +9,7 @@ NEI_sampling <- NEI[sample(nrow(NEI), size=2000, replace=F), ]
 
 # Aggregate
 
-Emissions <- aggregate(NEI[, 'Emissions'], by=list(NEI$year), FUN=sum)
+Emissions <- aggregate(NEI[, 'Emissions'],  by=list(NEI$year), FUN=sum)
 Emissions$PM <- round(Emissions[,2]/1000,2)
 
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
